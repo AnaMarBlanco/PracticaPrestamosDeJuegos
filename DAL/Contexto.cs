2b4a4b5b30b9edb32b342ba52c0f7c637b3cc509
panda <panda@DESKTOP-BLRQCTT>
2020-08-12 03:32:33 +0200

    Creando el contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PracticaPrestamosDeJuegos.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticaPrestamosDeJuegos.DAL
+{
+    class Contexto: DbContext
+    {
+        public DbSet<Amigos> Amigos { get; set; }
+        public DbSet<Juegos> Juegos { get; set; }
+        public DbSet<Prestamos> Prestamos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = Data/Database.db");
+        }
+    }
+}
